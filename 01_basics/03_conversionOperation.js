ca408b3c62feb2cde6fbf0b1b5e571e8cebb30b4
Ashish2533 <ashishjha2024.aj@gmail.com>
2024-09-29 06:08:48 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33absv"
+let score2 = null
+let score3 = undefined
+let score4 = true
+let score5 = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score4)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof score4)
+
+// "33abs" => NaN
+// true = 1; false = 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn) // For conversion we can write boolean, string number like in the code
+// console.log(booleanIsLoggedIn);
+
+// Conclusion
+// "" => false
+// "Hitesh" => true
+
+let someNumber =33
+let stringSomeNumber = String (someNumber)
+console.log(typeof stringSomeNumber);
+
+
