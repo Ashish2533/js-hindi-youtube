e42dea5b35669bbf54dc3d29e671530d6b6a2524
Ashish2533 <ashishjha2024.aj@gmail.com>
2024-09-29 05:42:59 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; /* treat all JS Code as newer version (In new Updates of JS we will write this so 
+ it indicates new functions, classes, and many JS concepts.) */
+
+//  alert(3 + 3) // we are using NodeJs its give error here and it works in Browser Console
+
+// code readability must high for other people also
+
+let name ="hitesh" // String
+let age = 18  // number
+let isLoggedIn = true // boolean
+
+
+//number => 2 to power 53
+//bigint
+// string => ""
+// boolean => True or False
+// null
+// undefined
+// symbol 
+
+// object
+
+console.log(typeof "Hitesh")
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
